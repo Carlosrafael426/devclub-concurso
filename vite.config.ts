983ee51dae862e382fae070b4b68aae9b0109f7e
Carlos Rafael <154095459+Carlosrafael426@users.noreply.github.com>
2026-08-03 22:35:13 -0300

    chore: set Vite base and build output to docs for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/devclub-concurso/',
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
